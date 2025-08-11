9a3b62a74c8b36d790db5b65999f7b62261821e7
robert-lee12 <mcgrathkayla612@gmail.com>
2025-08-11 21:38:02 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Confused)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Yes)
+})
